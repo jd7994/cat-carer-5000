1e8e7a7d976b9c3cdc3fd0ed34d1df4da93a1ad9
Jon <jderbyshire67@gmail.com>
2022-06-08 20:46:43 +0000

    Somewhat mended testing JD

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,75 +12,65 @@ class TestBase(TestCase):
                 )
         return app
 
-def setUp(self):
-    db.create_all()
-    test_cat = Cats(cat_name="Jumbo-Puss, Pigeon Eater")
-    test_food = Food(food="A whole pigeon")
-    db.session.add(test_cat, test_food)
-    db.session.commit()
+    def setUp(self):
+        db.create_all()
+        test_cat = Cats(cat_name="Jumbo-Puss, Pigeon Eater")
+        test_food = Food(food="A whole pigeon")
+        db.session.add(test_cat, test_food)
+        db.session.commit()
 
-def tearDown(self):
-    db.session.remove()
-    db.drop_all()
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
 class TestView(TestBase):  
     def test_home_get(self):
         response = self.client.get(url_for('home'))
         self.assertEqual(response.status_code, 200)
 
-#     def test_add_cat(self):
-#         response = self.client.get(url_for('add_cat'))
-#         self.assertEqual(response.status_code, 200)     
+    def test_add_cat(self):
+        response = self.client.get(url_for('add_cat'))
+        self.assertEqual(response.status_code, 200)     
 
-#     def test_add_food(self):
-#         response = self.client.get(url_for('add_food'))
-#         self.assertEqual(response.status_code, 200)
+    def test_add_food(self):
+        response = self.client.get(url_for('add_food'))
+        self.assertEqual(response.status_code, 200)
 
-#     def test_added(self):
-#         response = self.client.get(url_for('added'))
-#         self.assertEqual(response.status_code, 200)    
-
-#     def test_added_cat(self):
-#         response = self.client.get(url_for('added_cat'))
-#         self.assertEqual(response.status_code, 200)
-
-#     def test_edit_cat(self):
-#         response = self.client.get(url_for('edit_cat') + "/0")
-#         self.assertEqual(response.status_code, 200)
-
-#     def test_delete_cat(self):
-#         response = self.client.get(url_for('delete') + "/0")
-#         self.assertEqual(response.status_code, 200)
+    def test_added(self):
+        response = self.client.get(url_for('added'))
+        self.assertEqual(response.status_code, 200)    
 
 
-# class TestAdd(TestBase):
-#     def test_add_cat(self):
-#         response=self.client.post(
-#             url_for('add_cat'), 
-#             data = dict(cat_name="Big Susan"),
-#             follow_redirects = True
-#         )
-#         self.assert200(response)
-#         self.assertIn(b'Big Susan', response.data)
+class TestAdd(TestBase):
+    def test_add_cat(self):
+        response=self.client.post(
+            url_for('add_cat'), 
+            data = dict(cat_name="Big Susan"),
+            follow_redirects = True
+        )
+        self.assert200(response)
+        self.assertIn(b'Big Susan', response.data)
 
-#     def test_add_food(self):
-#         response=self.client.post(
-#             url_for('add_food'),
-#             data = dict(food="Carrot")
-#         )
-#         assert Food.query.filter_by(food="Carrot").id == 2
+    def test_add_food(self):
+        response=self.client.post(
+            url_for('add_food'),
+            data = dict(food="Carrot")
+        )
+        self.assert200(response)
+        self.assertIn(b'Carrot', response.data)
 
-# class TestDelete(TestBase):
-#     def test_delete_cat(self):
-#         response = self.client.delete(
-#             url_for('delete_cat'),
-#             data = dict(cat_name='Jumbo-Puss, Pigeon Eater')
-#         )
-#         assert len(Cat.query.all()) == 0
-    
-# #     def test_delete_food(self):
-# #         response = self.client.delete(
-# #             url_for('delete_food'),
-# #             data = dict(food="A whole pigeon")
-# #         )
-# #         assert len(Food.query.all()) == 0
+class TestDelete(TestBase):
+    def test_delete_cat(self):
+        url = str(url_for('delete')+"/0")
+        response = self.client.delete(
+            url,
+            data = dict(cat_name='Jumbo-Puss, Pigeon Eater')
+        )
+        assert len(Cat.query.all()) == 0
+#I don't think we have functionality to delete food actually??? #todo
+    def test_delete_food(self):
+        response = self.client.delete(
+            url_for('delete_food'),
+            data = dict(food="A whole pigeon")
+        )
+        assert len(Food.query.all()) == 0
